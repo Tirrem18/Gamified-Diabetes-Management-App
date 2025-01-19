06f5c7f52e6fee680ddaadd9d67a4f14d552f569
Tirrem <jacob.merritt18@gmail.com>
2025-01-19 10:43:22 +0000

    commit

---
+++ My Application/app/src/main/java/com/b1097780/glucohub/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.b1097780.glucohub.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ My Application/app/src/main/java/com/b1097780/glucohub/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.b1097780.glucohub.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
